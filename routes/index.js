e70f08492b32904941f1171e97305abc33435e6a
Conor <ckosidowski11@winona.edu>
2017-04-23 20:00:59 -0500

    Pulling the zip data from nvd

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+var http = require('https');
+var fs = require('fs');
+var AdmZip = require('adm-zip');
+
 router.get("/", function (req, res, next) {
     res.render("index", {title: "Gro"});
+
+    var file = fs.createWriteStream("out.json.zip");
+    var request = http.get("https://static.nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0-recent.json.zip", function(response) {
+        response.pipe(file);
+
+        response.on("end", function() {
+            var zip = new AdmZip("./out.json.zip");
+
+            zip.extractAllTo("./out", true);
+        })
+    });
 });
 
 router.get("/:name", function (req, res, next) {
